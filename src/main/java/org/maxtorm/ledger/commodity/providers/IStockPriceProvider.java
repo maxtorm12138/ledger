937f74d5624a9679e13cf2ae477d8a8ffa77d9c9
maxzwliao <maxzwliao@tencent.com>
2023-04-25 10:38:48 +0800

    feature: rename

--- src/main/java/org/maxtorm/ledger/commodity/providers/IStockPriceProvider.java
+++ src/main/java/org/maxtorm/ledger/commodity/providers/IStockPriceProvider.java
@@ -1,11 +1,12 @@
 package org.maxtorm.ledger.commodity.providers;
 
-import org.maxtorm.ledger.commodity.Commodity;
+import org.maxtorm.ledger.commodity.Currency;
+import org.maxtorm.ledger.commodity.Stock;
 import org.maxtorm.ledger.commodity.exception.CommodityProviderNetworkFailException;
 import org.springframework.data.util.Pair;
 
 import java.math.BigDecimal;
 
 public interface IStockPriceProvider {
-    Pair<Commodity, BigDecimal> getStockPrice(Commodity stock) throws CommodityProviderNetworkFailException;
+    Pair<Currency, BigDecimal> getStockPrice(Stock stock) throws CommodityProviderNetworkFailException;
 }
