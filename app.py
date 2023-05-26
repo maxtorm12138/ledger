8c778d340d5a1552abb566435652e16379b79778
maxzwliao <maxzwliao@tencent.com>
2023-05-26 13:48:55 +0800

    feat: currency quote

--- app.py
+++ app.py
@@ -35,18 +35,24 @@ class Quote:
         }
         return d
 
+
 class CurrencyRate:
-    from_amount: float
-    to_amount: float
+    from_amount: float = None
+    to_amount: float = None
+    isodate: datetime.date = None
     success: bool = False
     errormsg: str = ''
 
     def to_dict(self) -> dict:
         d = {
+            'from_amount': self.from_amount,
+            'to_amount': self.to_amount,
+            'isodate': self.isodate.strftime('%Y-%m-%d') if self.isodate is not None else None,
             'success': self.success,
             'errormsg': self.errormsg
         }
         return d
+
 
 @app.route('/fund_quote')
 def get_fund_quote():
@@ -74,18 +80,21 @@ def get_security_quote():
     if os.environ['PROXY_URL']:
         http = urllib3.ProxyManager(proxy_url=os.environ['PROXY_URL'])
 
-    http_request = http.request(method='GET', url='https://query1.finance.yahoo.com/v6/finance/quote?symbols=' + code)
+    http_request = http.request(method='GET',
+                                url='https://query2.finance.yahoo.com/v11/finance/quoteSummary/{}?modules=price,summaryDetail,defaultKeyStatistics'.format(
+                                    code))
+
     if http_request.status != 200:
         quote.success = False
         quote.errormsg = 'http fail {}'.format(http_request.status)
         return quote.to_dict()
 
     data = json.loads(http_request.data.decode('utf-8'))
-    data = data['quoteResponse']['result'][0]
+    data = data['quoteSummary']['result'][0]['price']
     app.logger.debug('response: %s', data)
 
     quote.currency = data['currency']
-    quote.last = data['regularMarketPrice']
+    quote.last = data['regularMarketPrice']['raw']
     quote.isodate = datetime.datetime.fromtimestamp(data['regularMarketTime'])
 
     quote.success = True
@@ -103,11 +112,24 @@ def get_currency_quote():
     if os.environ['PROXY_URL']:
         http = urllib3.ProxyManager(proxy_url=os.environ['PROXY_URL'])
 
-    http_request = http.request(method='GET', url='https://query1.finance.yahoo.com/v6/finance/quote?symbols={}{}%3DX'.format(from_currency, to_currency))
+    http_request = http.request(method='GET',
+                                url='https://query2.finance.yahoo.com/v11/finance/quoteSummary/{}{}=X?modules=price,summaryDetail,defaultKeyStatistics'.format(
+                                    from_currency, to_currency))
     if http_request.status != 200:
         currency_rate.success = False
         currency_rate.errormsg = 'http fail {}'.format(http_request.status)
         return currency_rate.to_dict()
+
+    data = json.loads(http_request.data.decode('utf-8'))
+    data = data['quoteSummary']['result'][0]['price']
+    app.logger.debug('response: %s', data)
+
+    currency_rate.from_amount = 1.0
+    currency_rate.to_amount = data['regularMarketPrice']['raw']
+    currency_rate.isodate = datetime.datetime.fromtimestamp(data['regularMarketTime'])
+    currency_rate.success = True
+
+    return currency_rate.to_dict()
 
 
 if __name__ == '__main__':
