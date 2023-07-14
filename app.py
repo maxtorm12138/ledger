aaea41badf5b7b398d18f2334311a189344a9665
maxtorm <maxtorm12138@gmail.com>
2023-07-14 12:55:55 +0800

    feat: fix quote source

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ import os
 import urllib3
 from flask import Flask, request
 import akshare as ak
+import yfinance as yf
+from lxml import etree
+import re
 
 app = Flask(__name__)
 
 
 class Quote:
+    def __init__(self):
+        pass
+
     name: str = None
     last: float = None
     isodate: datetime.date = None
@@ -72,30 +78,46 @@ def get_security_quote():
 
     quote = Quote()
 
+
     http = urllib3.PoolManager()
     if os.environ.get('PROXY_URL'):
         http = urllib3.ProxyManager(proxy_url=os.environ['PROXY_URL'])
 
     http_request = http.request(method='GET',
-                                url='https://query2.finance.yahoo.com/v11/finance/quoteSummary/{}?modules=price,summaryDetail,defaultKeyStatistics'.format(
-                                    code))
-
+                                url='https://finance.yahoo.com/quote/{}/history?p={}'.format(
+                                    code, code), headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'})
     if http_request.status != 200:
         quote.success = False
         quote.errormsg = 'http fail {}'.format(http_request.status)
         return quote.to_dict()
 
-    data = json.loads(http_request.data.decode('utf-8'))
-    data = data['quoteSummary']['result'][0]['price']
-    app.logger.debug('response: %s', data)
-
-    quote.currency = data['currency']
-    quote.last = data['regularMarketPrice']['raw']
-    quote.isodate = datetime.datetime.fromtimestamp(data['regularMarketTime'])
-
+    data = etree.fromstring(http_request.data.decode('utf-8'), etree.HTMLParser())
+    quote.name = re.search('^(.+) \(([^)]+)\)', data.xpath('//*[@id="quote-header-info"]//div//h1')[0].text).group(1)
+    quote.currency = re.search('^(.+)[.] Currency in (.+)$', data.xpath('//*[@id="quote-header-info"]//div//span')[0].text).group(2)
+    quote.last = float(data.xpath('//*[@data-symbol="{}"][@data-field="regularMarketPrice"]'.format(code))[0].text)
+    quote.isodate = datetime.datetime.strptime(data.xpath('//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table/tbody/tr[1]/td[1]/span')[0].text, '%b %d, %Y')
     quote.success = True
 
-    app.logger.info(quote.to_dict())
+    # http = urllib3.PoolManager()
+    # if os.environ.get('PROXY_URL'):
+    #     http = urllib3.ProxyManager(proxy_url=os.environ['PROXY_URL'])
+
+    # http_request = http.request(method='GET',
+    #                             url='https://query2.finance.yahoo.com/v11/finance/quoteSummary/{}?modules=price,summaryDetail,defaultKeyStatistics'.format(
+    #                                 code))
+
+
+    # data = json.loads(http_request.data.decode('utf-8'))
+    # data = data['quoteSummary']['result'][0]['price']
+    # app.logger.debug('response: %s', data)
+
+    # quote.currency = data['currency']
+    # quote.last = data['regularMarketPrice']['raw']
+    # quote.isodate = datetime.datetime.fromtimestamp(data['regularMarketTime'])
+
+    # quote.success = True
+
+    # app.logger.info(quote.to_dict())
     return quote.to_dict()
 
 
